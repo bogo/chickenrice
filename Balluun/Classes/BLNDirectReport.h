3d6cae8fa6dc5bee74e9db52cb7e9a4d6d78a7b5
Jeremy Foo <jeremyfoo@gmail.com>
2015-07-11 19:24:29 -0700

    Direct reports report to the manager. Duh.

---
+++ Balluun/Classes/BLNDirectReport.h
@@ -0,0 +1,15 @@
+//
+//  BLNDirectReport.h
+//  Balluun
+//
+//  Created by Jeremy Foo on 11/7/15.
+//  Copyright © 2015 Ottoman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLNDirectReport : NSObject
+
++ (instancetype)sharedInstance;
+
+@end
---
+++ Balluun/Classes/BLNDirectReport.m
@@ -0,0 +1,13 @@
+//
+//  BLNDirectReport.m
+//  Balluun
+//
+//  Created by Jeremy Foo on 11/7/15.
+//  Copyright © 2015 Ottoman. All rights reserved.
+//
+
+#import "BLNDirectReport.h"
+
+@implementation BLNDirectReport
+
+@end
